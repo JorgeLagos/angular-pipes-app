ba975c9ab01c480dcc777ba9f55a3089c399b827
Jorge Lagos <jor.lagos.1988@gmail.com>
2025-08-13 09:44:28 -0400

    add: keyvalue pipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,18 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
-import { I18nPluralPipe, I18nSelectPipe, JsonPipe, SlicePipe, UpperCasePipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 const client1 = { name: 'Jorge', gender: 'male', age: 36, address: 'Santiago, Chile' };
 const client2 = { name: 'Melissa', gender: 'female', age: 33, address: 'Renca, Chile' };
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe, UpperCasePipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe, UpperCasePipe, KeyValuePipe, TitleCasePipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
 
-  // i18n Select
+  // I18nSelectPipe
   public client = signal(client1);
   public invitationMap = { male: 'invitarlo', female: 'invitarla' };
 
@@ -25,12 +25,19 @@ export default class UncommonPageComponent {
     this.client.set(client1);
   }
 
-  // i18n Plural
+  // I18nPluralPipe
   public clients = signal(['Jorge', 'Carmen', 'Maria', 'Fernando', 'Melissa', 'Natalia', 'Juan', 'Carlos']);
   public clientsMap = { '=0': 'no tenemos ningún cliente esperando', '=1': 'tenemos un cliente esperando', other: 'tenemos # clientes esperando'};
 
   public deleteClient() {
     this.clients.update((prev) => prev.slice(1));
+  }
+
+  // KeyValuePipe
+  public profile = {
+    name: 'Jorge',
+    age: 36,
+    address: 'Santiago, Chile'
   }
 
 }
